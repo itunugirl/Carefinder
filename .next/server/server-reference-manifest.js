19dc4cbd9414af04c8e457a0f9017ffff923dec6
itunugirl <itunuoluwafelicia@gmail.com>
2024-08-15 09:34:33 -0700

    change master

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"g2yRJbrApDaTYDJJ4enp3oGHifm/rBgsav/wBp2qNPM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3RcCh0X4p4RUJ3X+hgu+RT2NG80m3pMv+69U7dh4T80=\"\n}"
