7f8cecb0540f17a34afcb85c559e350809a301b9
itunugirl <itunuoluwafelicia@gmail.com>
2024-08-15 16:01:15 -0700

    my code

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 const path = require('path');
 
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'fonts.googleapis.com',
-        port: '',
-        pathname: '/css2?family=Satoshi:wght@100;200;300;400;500;600;700;800;900&display=swap',
-      },
-    ],
-  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@firebaseConfig': path.resolve(__dirname, 'src/firebaseConfig'),
-      'metaData': path.resolve(__dirname, 'src/data/metaData') // Adjust the path as needed
+      '@firebaseConfig': path.resolve(__dirname, 'src/firebaseConfig.ts'),
+      'metaData': path.resolve(__dirname, 'src/data/metaData'),
     };
     return config;
   },
