ef27aa0c5dfb6d24132338038805f3190aaf030d
itunugirl <itunuoluwafelicia@gmail.com>
2024-08-16 02:48:45 -0700

    my code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 const path = require('path');
 
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fonts.googleapis.com',
+        port: '',
+        pathname: '/css2?family=Open+Sans:wght@300;400&display=swap',
+      },
+      {
+        protocol: 'https',
+        hostname: '', // Update this if needed
+        port: '',
+        pathname: '', // Adjust as needed
+      },
+    ],
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
