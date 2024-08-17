577907fb1be79d19335d96783c6b99689ec03bda
itunugirl <itunuoluwafelicia@gmail.com>
2024-08-17 05:48:27 -0700

    my code

---
+++ src/app/services/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      {/* Your component code */}
+    </div>
+  );
+};
+
+export default Page;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,20 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Satoshi:wght@100;200;300;400;500;600;700;800;900&display=swap" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+          {/* Google Fonts */}
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          />
+          {/* Font Awesome */}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+          />
         </Head>
         <body>
           <Main />
